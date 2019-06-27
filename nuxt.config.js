f4c94973acf8960e9b7dde3b66a2fb914fb6e3ea
mismith0227 <doraepon2216@gmail.com>
2019-06-27 19:52:51 +0900

    修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,11 @@ module.exports = {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Nuxt.js project" }
+      { hid: "description", name: "description", content: "Nuxt.js project" },
+      {
+        name: "google-site-verification",
+        content: "p66tw3S0Pws_FfywKFt06jbDvzDE4v4IkgupQKLLeoM"
+      }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
